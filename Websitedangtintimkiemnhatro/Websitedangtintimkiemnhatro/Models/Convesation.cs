b608db5ec74863be214d8549bea7c5ca1e9117bf
lampham9 <43400884+lampham9@users.noreply.github.com>
2020-11-03 11:19:54 +0700

    Back end database

---
+++ Websitedangtintimkiemnhatro/Websitedangtintimkiemnhatro/Models/Convesation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Websitedangtintimkiemnhatro.Models
+{
+    public class Convesation
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+        public string Status { get; set; }
+        public User UserOne { get; set; }
+        [ForeignKey("UserOneId")]
+        [Required]
+        public int UserOneId { get; set; }
+
+        public User UserTwo { get; set; }
+        [ForeignKey("UserTwoId")]
+        [Required]
+        public int UserTwoId { get; set; }
+
+        public ICollection<Convesation_reply> Convesation_replys { get; set; }
+
+    }
+}
