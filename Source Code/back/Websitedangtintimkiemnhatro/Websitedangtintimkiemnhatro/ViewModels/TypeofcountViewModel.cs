2a0823a769c6ed2aa6447544888f7e1578c3cf26
PhamHuynhThanhLam <43400884+PhamHuynhThanhLam@users.noreply.github.com>
2021-01-02 11:48:47 +0700

    new back 2

---
+++ Source Code/back/Websitedangtintimkiemnhatro/Websitedangtintimkiemnhatro/ViewModels/TypeofcountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Websitedangtintimkiemnhatro.Models;
+
+namespace Websitedangtintimkiemnhatro.ViewModels
+{
+    public class TypeofcountViewModel
+    {
+        public List<string> Typeofnews { get; set; }
+        public List<int> count { get; set; }
+    }
+}
